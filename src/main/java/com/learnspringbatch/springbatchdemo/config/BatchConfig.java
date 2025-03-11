90d3faf03f554aee0e6c00c4b311c437b63a7251
isaac_kwok <isaac4learn@gmail.com>
2025-03-10 21:49:29 -0400

    chuck processing

--- src/main/java/com/learnspringbatch/springbatchdemo/config/BatchConfig.java
+++ src/main/java/com/learnspringbatch/springbatchdemo/config/BatchConfig.java
@@ -1,5 +1,8 @@
 package com.learnspringbatch.springbatchdemo.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -8,10 +11,14 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.learnspringbatch.springbatchdemo.reader.ProductNameItemReader;
 
 @Configuration
 @EnableBatchProcessing
@@ -24,57 +31,91 @@ public class BatchConfig {
 	public StepBuilderFactory stepBuilderFactory;
 	
 	// Bean for custom decider and listener
-
+	
+	@Bean
+	public ItemReader<String> itemReader() {
+		List<String> productList = new ArrayList<>();
+		productList.add("Product_1");
+		productList.add("Product_2");
+		productList.add("Product_3");
+		productList.add("Product_4");
+		productList.add("Product_5");
+		return new ProductNameItemReader(productList);
+	}
+	
 	@Bean
 	public Step stepOne() {
-		return this.stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+		return this.stepBuilderFactory.get("chunkBasedStep1")
+				.<String,String>chunk(3) // process 3 items for 1 chuck
+				.reader(itemReader())
+				.writer(new ItemWriter<String>() {
 
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("step1 executed!!");
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
+					@Override
+					public void write(List<? extends String> items) throws Exception {
+						System.out.println("Chunk-processing Started");
+						items.forEach(System.out::println);
+						System.out.println("Chunk-processing Ended");
+					}
+				}).build();
 	}
 	
 	@Bean
-	public Step stepTwo() {
-		return this.stepBuilderFactory.get("step2").tasklet(new Tasklet() {
-
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				boolean isStepTwoSuccess = true;
-				if(isStepTwoSuccess) {
-					System.out.println("step2 executed!!");
-				}
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
+	public Job firstJob() {
+		return this.jobBuilderFactory.get("job1")
+				.start(stepOne())
+				.build();
 	}
+
+//	@Bean
+//	public Step stepOne() {
+//		return this.stepBuilderFactory.get("step1").tasklet(new Tasklet() {
+//
+//			@Override
+//			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//				System.out.println("step1 executed!!");
+//				return RepeatStatus.FINISHED;
+//			}
+//		}).build();
+//	}
+//	
+//	@Bean
+//	public Step stepTwo() {
+//		return this.stepBuilderFactory.get("step2").tasklet(new Tasklet() {
+//
+//			@Override
+//			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//				boolean isStepTwoSuccess = true;
+//				if(isStepTwoSuccess) {
+//					System.out.println("step2 executed!!");
+//				}
+//				return RepeatStatus.FINISHED;
+//			}
+//		}).build();
+//	}
+//	
+//	@Bean
+//	public Step stepThree() {
+//		return this.stepBuilderFactory.get("step3").tasklet(new Tasklet() {
+//
+//			@Override
+//			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//				System.out.println("step3 executed!!");
+//				return RepeatStatus.FINISHED;
+//			}
+//		}).build();
+//	}
 	
-	@Bean
-	public Step stepThree() {
-		return this.stepBuilderFactory.get("step3").tasklet(new Tasklet() {
-
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("step3 executed!!");
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
-	}
-	
-	@Bean
-	public Step stepFour() {
-		return this.stepBuilderFactory.get("step4").tasklet(new Tasklet() {
-
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("step4 executed!!");
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
-	}
+//	@Bean
+//	public Step stepFour() {
+//		return this.stepBuilderFactory.get("step4").tasklet(new Tasklet() {
+//
+//			@Override
+//			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//				System.out.println("step4 executed!!");
+//				return RepeatStatus.FINISHED;
+//			}
+//		}).build();
+//	}
 
 	// .preventRestart - stop re-execute
 	// sequence stepping
@@ -91,13 +132,13 @@ public class BatchConfig {
 	// listener package for custom condition
 	
 	// step-by-step batch
-	@Bean
-	public Job firstJob() {
-		return this.jobBuilderFactory.get("job1")
-				.preventRestart()
-				.start(stepOne())
-				.next(stepTwo())
-				.next(stepThree())
-				.build();
-	}
+//	@Bean
+//	public Job firstJob() {
+//		return this.jobBuilderFactory.get("job1")
+//				.preventRestart()
+//				.start(stepOne())
+//				.next(stepTwo())
+//				.next(stepThree())
+//				.build();
+//	}
 }
---
+++ src/main/java/com/learnspringbatch/springbatchdemo/reader/ProductNameItemReader.java
@@ -0,0 +1,29 @@
+package com.learnspringbatch.springbatchdemo.reader;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class ProductNameItemReader implements ItemReader<String>{
+
+	private Iterator<String> productListIterator;
+	
+	public ProductNameItemReader(List<String> productList) {
+		this.productListIterator = productList.iterator();
+	}
+	
+	@Override
+	public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+		if(this.productListIterator.hasNext()) {
+			return this.productListIterator.next();
+		} else {
+			return null;
+		}
+	}
+
+	
+}
